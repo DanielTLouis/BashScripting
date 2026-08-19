d789af59aff3e0f141bf2f32b49b7a23f0b3065b
Daniel <llouis5314@gmail.com>
2026-08-19 09:14:22 -0400

    Updated ubuntu_system_into.sh to add some formatting and prints out the current user

--- ubuntu_system_info.sh
+++ ubuntu_system_info.sh
@@ -3,9 +3,20 @@
 # Created: 08/14/2026
 # This Bash Script will output system information for systems running Ubuntu
 
+#output current user 
+echo -e "Current User: $USER"
+
+# Give some space
+echo 
+
 #Prints the ubuntu version 
+echo "Ubuntu Release:"
 lsb_release -a 
+
+# Give some space
+echo 
 
 
 # Prints toatl amount of usable memory for the system 
+echo "Memory:"
 free -h | awk '/^Mem:/ {print "Total Available memory:", $2}'
