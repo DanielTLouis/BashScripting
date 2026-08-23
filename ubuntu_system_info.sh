39b1fa05801a000805eb4f67f3876a0a5c32ddda
Daniel <llouis5314@gmail.com>
2026-08-23 08:03:32 -0400

    Updated system info bash script to add more structure and functaitnoality in the system output.

--- ubuntu_system_info.sh
+++ ubuntu_system_info.sh
@@ -3,20 +3,73 @@
 # Created: 08/14/2026
 # This Bash Script will output system information for systems running Ubuntu
 
+## !! Run this in Sublime Editor with 'CTRL + B'
+
 #output current user 
-echo -e "Current User: $USER"
+#echo -e "Current User: $USER"
 
 # Give some space
-echo 
+#echo 
 
 #Prints the ubuntu version 
 echo "Ubuntu Release:"
 lsb_release -a 
 
 # Give some space
-echo 
+#echo 
 
 
-# Prints toatl amount of usable memory for the system 
-echo "Memory:"
-free -h | awk '/^Mem:/ {print "Total Available memory:", $2}'
+# Prints total amount of usable memory for the system 
+#echo "Memory:"
+#free -h | awk '/^Mem:/ {print "Total Available memory:", $2}'
+
+# Prints the search path to indicate where applications are located 
+#echo $PATH
+
+
+system_info()
+{
+	echo "==============================================="
+	echo "=           UBUNTU SYSTEM INFORMATION         ="
+	echo "==============================================="
+
+	echo 
+	echo "[ SYSTEM ]"
+	echo 
+	#Distribution name 
+	distribution=$(lsb_release -a 2>/dev/null | grep "Distributor" | cut -f2)
+		# 2>/dev/null will send the error stream to null instead of stdout, to avoid the line "No LSB modules are available." from being printed
+	echo "Distribution Name: $distribution"
+	#Ubuntu version
+	version=$(lsb_release -a 2>/dev/null | grep "Description" | cut -f2)
+	echo "Version: $version"
+	#Release/codename
+	release=$(lsb_release -a 2>/dev/null | grep "Release" | cut -f2)
+	echo "Release: $release"
+	#Kernel version
+	kernel=$(uname -r)
+	echo "Kernel Version: $kernel"
+	#System architecture
+	sys_arc=$(uname -m)
+	echo "System Architecture: $sys_arc"
+	#Hostname
+	hostname=$(hostname)
+	echo "Hostname: $hostname"
+	#System uptime
+	uptime=$(uptime | cut -d, -f1)
+	echo "System Uptime: $uptime"
+
+	echo 
+	echo "[ HARDWARE ]"
+	echo 
+
+	echo 
+	echo "[ MEMORY ]"
+	echo 
+
+	echo
+	echo "[ STORAGE ]"
+	echo 
+}
+
+system_info 
